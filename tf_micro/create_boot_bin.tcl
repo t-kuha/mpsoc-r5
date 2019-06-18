92da43d3dd7bff03760c0622f60cd514d7273e1b
t_kuha <imagingtechnerd@gmail.com>
2019-06-18 12:48:53 +0900

    Add tcl scripts for creating base SW

---
+++ tf_micro/create_boot_bin.tcl
@@ -0,0 +1,52 @@
+# Create BOOT.bin (for standalone)
+# 
+# Usage: In Xilinx Software Command Line Tool, run the following command:
+#           source create_boot_bin.tcl
+# 
+# Need to install board files in <SDK Installation Path>/data/boards/board_files
+#
+# Reference: UG1208
+# 
+
+set XSDK_DIR    sdk
+set PRJ_NAME    u96_tf
+set SRC_DIR     src
+
+set SCRIPT_DIR [ file dirname [ file normalize [ info script ] ] ]
+
+cd ${SCRIPT_DIR}
+
+setws ${SCRIPT_DIR}/${XSDK_DIR}
+
+# Create BSP
+createhw -name bsp -hwspec ${XSDK_DIR}/${PRJ_NAME}.hdf
+
+# Create FSBL
+createapp -name fsbl -app {Zynq MP FSBL} -hwproject bsp -proc psu_cortexr5_0 -os standalone
+
+# Create application
+createapp -name tf -app {Hello World} -hwproject bsp -proc psu_cortexr5_0 -os standalone
+
+# Use UART1 as stdin & stdout 
+configbsp -bsp fsbl_bsp stdin  "psu_uart_1"
+configbsp -bsp fsbl_bsp stdout "psu_uart_1"
+updatemss -mss ${XSDK_DIR}/fsbl_bsp/system.mss
+regenbsp -bsp fsbl_bsp
+
+configbsp -bsp tf_bsp stdin  "psu_uart_1"
+configbsp -bsp tf_bsp stdout "psu_uart_1"
+updatemss -mss ${XSDK_DIR}/tf_bsp/system.mss
+regenbsp -bsp tf_bsp
+
+# Replace helloworld.c with led_blink.c
+# exec rm ${XSDK_DIR}/hello_world/${SRC_DIR}/helloworld.c
+# exec cp ${SRC_DIR}/helloworld.c ${XSDK_DIR}/hello_world/${SRC_DIR}/helloworld.c
+
+# Build in release mode
+configapp -app fsbl build-config release
+configapp -app tf build-config release
+
+projects -build
+
+# Generate BOOT.bin
+# exec bootgen -arch zynqmp -image ${SRC_DIR}/boot_bin.bif -w -o BOOT.bin
---
+++ tf_micro/create_vivado_project.tcl
@@ -0,0 +1,35 @@
+# Setting
+set PRJ_DIR     _vivado
+set PRJ_NAME    u96_tf
+set BD_NAME     design_1
+set SRC_DIR     src
+set XSDK_DIR    sdk
+set NUM_JOBS    4
+
+
+# Create project
+create_project ${PRJ_NAME} ${PRJ_DIR} -part xczu3eg-sbva484-1-e
+set_property board_part em.avnet.com:ultra96v1:part0:1.2 [current_project]
+
+# Create block design
+source ${SRC_DIR}/bd.tcl
+
+# Set top-level source
+make_wrapper -files [get_files ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd] -top
+add_files -norecurse ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/hdl/${BD_NAME}_wrapper.v
+set_property top ${BD_NAME}_wrapper [current_fileset]
+update_compile_order -fileset sources_1
+
+# Generate block design
+regenerate_bd_layout
+validate_bd_design
+save_bd_design
+generate_target all [get_files  ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd]
+
+# We do not have to generate bitstream
+
+# Export HW (.gdf file) for SDK
+write_hwdef -force -file ${XSDK_DIR}/${PRJ_NAME}.hdf
+
+# Finish - close project
+close_project
